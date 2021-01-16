860b8eaf16c41ef53bd08ca34d42086df5214752
evan999 <emeshberge@gmail.com>
2021-01-16 16:18:06 -0500

    Initialize BlackJack CardGame project. Initial commit.

---
+++ src/com/oop/projects/CardGames/Card.java
@@ -0,0 +1,13 @@
+package com.oop.projects.CardGames;
+
+public class Card {
+    private int value;
+    private String suit;
+
+    public Card(int value, String suit){
+        this.value = value;
+        this.suit = suit;
+    }
+
+    
+}
